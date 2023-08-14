fd4d58101853c1a43cd493b902dd046d88a76e2a
feliperod0519@hotmail.com <juan_andres_rodriguez_0519@hotmail.com>
2023-08-13 23:13:25 -0400

    Some service testing

--- app/services/authentication.service.spec.ts
+++ app/services/authentication.service.spec.ts
@@ -1,16 +1,53 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AuthenticationService } from './authentication.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { first } from 'rxjs/operators';
+import { Person } from '../models/person';
 
 describe('AuthenticationService', () => {
-  let service: AuthenticationService;
+  let authenticationService: AuthenticationService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthenticationService);
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule],
+      providers:[AuthenticationService]
+    });
+    authenticationService = TestBed.inject(AuthenticationService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  fit('should log in', () => {
+    //expect(service).toBeTruthy();
+    authenticationService.login('minou@cat-tv.ca','hello').pipe(first()).subscribe((x)=>{
+      expect(x).toBeTruthy();
+    });
+    const req = httpTestingController.expectOne("http://localhost:8000/api/authenticate");
+    expect(req.request.method).toEqual("POST");
+    expect(req.request.body.email).toEqual('minou@cat-tv.ca');
+    expect(req.request.body.password).toEqual('hello');
+    req.flush({...{"id":0,"email":'',"bio":""},...{"id":1,"email":'minou@cat-tv.ca',"bio":'soy un gato'}})
+  });
+
+  fit('should give an error if login info is incorrect',()=>{
+    authenticationService.login('minou@cat-tv.ca','hello-').pipe(first()).subscribe(
+      ()=>{fail('login failed');},
+      (err:HttpErrorResponse)=>{ expect(err.status).toBe(403); }
+    );
+    const req = httpTestingController.expectOne("http://localhost:8000/api/authenticate");
+    expect(req.request.method).toEqual("POST");
+    expect(req.request.body.email).toEqual('minou@cat-tv.ca');
+    expect(req.request.body.password).toEqual('hello-');
+    req.flush('Login failed', {
+                                      status:403,
+                                      statusText:'Unauthenticated'           
+                              });
+  });
+
+  afterEach(()=>{
+    httpTestingController.verify();
   });
 });
+
